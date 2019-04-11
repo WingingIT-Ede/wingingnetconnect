453a1ed12852c57cfb9a2f05543ea602157db29c
Wouter <wouter@wshovius.nl>
2019-04-11 16:53:17 +0200

    add function

--- src/HoviusNetConnect.php
+++ src/HoviusNetConnect.php
@@ -2,6 +2,8 @@
 
 namespace Wshovius\HoviusNetConnect;
 
+use GuzzleHttp\Client;
+
 class HoviusNetConnect
 {
     public static function login(){
@@ -12,11 +14,28 @@ class HoviusNetConnect
 
     }
 
-    public static function checkAccount(){
+    public static function checkAccount($code = null){
 
     }
 
     public static function drawConnectButton(){
         return view('wshovius::connectbutton');
+    }
+
+    public static function getAuthToken($code){
+        $http = new Client;
+
+        $response = $http->post('https://network.wshovius.nl/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'authorization_code',
+                'client_id' => config('hoviusnetconnect.client_id'),
+                'client_secret' => config('hoviusnetconnect.client_secret'),
+                'redirect_uri' => config('hoviusnetconnect.redirect_uri'),
+                'code' => $code,
+            ]
+        ]);
+
+        dd($response);
+        return $response;
     }
 }

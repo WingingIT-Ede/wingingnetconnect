baa5abaa567da743bc05cfce7fbd3acccd6dfd18
Wouter <wouter@wshovius.nl>
2019-04-11 18:02:19 +0200

    check something

--- src/HoviusNetConnect.php
+++ src/HoviusNetConnect.php
@@ -7,56 +7,71 @@ use GuzzleHttp\Client;
 class HoviusNetConnect
 {
     public static function login($token){
-        $http = new Client;
 
-        $response = $http->post('https://network.wshovius.nl/api/remotelogin', [
-            'header' => [
-                'Accept' => 'application/json',
-                'Authorization' => $token,
-            ],
-            'body' => json_encode([
-                'client_id' => config('hoviusnetconnect.client_id')
-            ])
-        ]);
-        if(!empty(json_decode((string) $response->getBody(), true)['error'])) {
+        $url = 'https://network.wshovius.nl/api/remotelogin';
+        $data = ['client_id' => '1'];
+
+// use key 'http' even if you send the request to https://...
+        $options = [
+            'http' => [
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\nAuthorization: ".$token."\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            ]
+        ];
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE || !empty(json_decode($result, true)['error']) || empty(json_decode($result, true)['login_reference'])) {
             return redirect()->route('login');
         }
-        return json_decode((string) $response->getBody(), true)['login_reference'];
+
+        return json_decode($result, true)['login_reference'];
+
+
     }
 
     public static function register($token){
-        $http = new Client;
+        $url = 'https://network.wshovius.nl/api/newregistration';
+            $data = ['client_id' => '1'];
 
-        $response = $http->post('https://network.wshovius.nl/api/newregistration', [
-            'header' => [
-                'Accept' => 'application/json',
-                'Authorization' => $token,
-            ],
-            'body' => json_encode([
-                'client_id' => config('hoviusnetconnect.client_id')
-            ])
-        ]);
-        if(!empty(json_decode((string) $response->getBody(), true)['error'])) {
+// use key 'http' even if you send the request to https://...
+        $options = [
+            'http' => [
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\nAuthorization: ".$token."\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            ]
+        ];
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE || !empty(json_decode($result, true)['error']) || empty(json_decode($result, true)['login_reference'])) {
             return redirect()->route('login');
         }
 
-        return json_decode((string) $response->getBody(), true)['login_reference'];
+        return json_decode($result, true)['login_reference'];
     }
 
     public static function checkAccount($token){
-        $http = new Client;
 
-        $response = $http->post('https://network.wshovius.nl/api/checkAccount', [
-            'header' => [
-                'Accept' => 'application/json',
-                'Authorization' => $token,
-            ],
-            'form_params' => json_encode([
-                'client_id' => config('hoviusnetconnect.client_id')
-            ])
-        ]);
-        dd($response->getBody());
-        return (bool)json_decode((string) $response->getBody(), true)['existing_account'];
+
+        $url = 'http://network.wshovius.nl/api/checkAccount';
+        $data = ['client_id' => '1'];
+
+// use key 'http' even if you send the request to https://...
+        $options = [
+            'http' => [
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\nAuthorization: ".$token."\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            ]
+        ];
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE) {
+            return false;
+        }
+
+        return (bool)json_decode($result, true)['existing_account'];
     }
 
     public static function drawConnectButton(){

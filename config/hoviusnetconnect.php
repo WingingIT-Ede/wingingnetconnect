25158ea9d80948265abce59a75115e38cbde0aa3
Wouter <wouter@subspace.ga>
2019-09-16 15:31:18 +0200

    finish rebranding

--- config/hoviusnetconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'client_id' => env('HOVIUSNET_CLIENT_ID', null),
-    'client_secret' => env('HOVIUSNET_CLIENT_SECRET', null),
-    'redirect_uri' => env('HOVIUSNET_REDIRECT_URI', null),
-];
---
+++ config/wingingnetconnect.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'client_id' => env('WINGINGNET_CLIENT_ID', null),
+    'client_secret' => env('WINGINGNET_CLIENT_SECRET', null),
+    'redirect_uri' => env('WINGINGNET_REDIRECT_URI', null),
+];
